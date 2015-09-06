7d4e60b8804738dc759ee70b542975b596d05757
Michael Vilabrera <m.vilabrera@gmail.com>
2015-09-06 16:46:37 -0400

    initial commit

---
+++ BNRRandomPassword/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  BNRRandomPassword
+//
+//  Created by Michael Vilabrera on 9/6/15.
+//  Copyright (c) 2015 Giving Tree. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
